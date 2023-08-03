93fbf1f7a68f4479383c88e95bcf63cf543f3351
mannieshub <110074002+mannieshub@users.noreply.github.com>
2023-08-04 00:07:06 +0700

    Revert "Merge branch 'chatbot'"
    This reverts commit 0ade4274d3573ca052108da826c5e96383fe439d, reversing
    changes made to 8f7f817bce9e72529a142154ab6b226e535003af.

--- mobile_app/test/widget_test.dart
+++ mobile_app/test/widget_test.dart
@@ -7,13 +7,12 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:mobile_app/chatbot_page.dart';
+import 'package:mobile_app/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const chatbot());
+    await tester.pumpWidget(const MyApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
