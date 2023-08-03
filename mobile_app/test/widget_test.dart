e90deda62ef66fb45fe3d4752d0cf1558d8e4158
mannieshub <110074002+mannieshub@users.noreply.github.com>
2023-08-03 09:17:09 +0700

    chatbot version 1
    create chatbot_page.dart
    add assets - json
    add images

--- mobile_app/test/widget_test.dart
+++ mobile_app/test/widget_test.dart
@@ -8,12 +8,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:mobile_app/main.dart';
+import 'package:mobile_app/chatbot_page.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const chatbot());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
